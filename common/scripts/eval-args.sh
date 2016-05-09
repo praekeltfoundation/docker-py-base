7d901c4448943fd3ad6b465247feaaa0f204c4e7
Jamie Hewland <jhewland@gmail.com>
2016-05-09 15:27:04 +0200

    Try convert common shell scripts to sh

--- common/scripts/eval-args.sh
+++ common/scripts/eval-args.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/sh -e
+
 # The "default parameters to entrypoint" and "exec" forms of Docker's CMD
 # instruction don't run in a shell and therefore variables (e.g. $HOME) are not
 # evaluated. This script evaluates the CMD values in a shell and then replaces
 # the shell process with the evaluated command.
 for ORIG in "$@"; do
     PARAM=$(eval "echo ${ORIG}")
-    PARAMS+=("${PARAM}")
+    PARAMS="$PARAMS $PARAM"
 done
 
-exec "${PARAMS[@]}"
+exec $PARAMS
