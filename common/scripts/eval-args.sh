764edfb6c1c215064e3940e4ef3238c8cdeb4bb3
Jamie Hewland <jhewland@gmail.com>
2016-05-10 09:41:30 +0200

    Fix Bourne shell port of eval-args.sh

--- common/scripts/eval-args.sh
+++ common/scripts/eval-args.sh
@@ -4,9 +4,11 @@
 # instruction don't run in a shell and therefore variables (e.g. $HOME) are not
 # evaluated. This script evaluates the CMD values in a shell and then replaces
 # the shell process with the evaluated command.
-for ORIG in "$@"; do
-    PARAM=$(eval "echo ${ORIG}")
-    PARAMS="$PARAMS $PARAM"
+
+for i in $(seq 1 $#); do
+  eval "ARG$i=\$(eval echo \$$i)"
+  ARGS="${ARGS:+$ARGS }\"\$ARG$i\""
 done
 
-exec $PARAMS
+eval "set -- $ARGS"
+exec "$@"
